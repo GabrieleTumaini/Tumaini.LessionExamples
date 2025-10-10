3ee67b6d17391135835b54341b66f07d3b169e12
GabrieleTumaini <gabriele.tumaini.stud@ispascalcomandini.it>
2025-10-10 09:45:32 +0200

    Versione del 10/10

--- src/BlasePascal.LessonsExamples.Domain/Enemy.cs
+++ src/BlasePascal.LessonsExamples.Domain/Enemy.cs
@@ -15,12 +15,61 @@
         private int _positionY;
         private int _movementSpeed;
 
+        //Properties
+        //public int health
+        //{
+        //    get
+        //    {
+        //        return _health;
+        //    }
+
+        //    private set
+        //    {
+        //        if (value < 0)
+        //        {
+        //            throw new argumentoutofrangeexception("value");
+        //        }
+        //        _health = value;
+        //    }
+        //}
+
+        public int Health { get; private set; }
+
+        public bool isAlive { get; private set; }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int AtakDamage
+        {
+            get
+            {
+                return _attackDamage;
+            }
+
+            set
+            {
+                _attackDamage = value;
+            }
+        }
+
         // Constructor
         public Enemy() { }
         public Enemy(string name)
         {
             SetName(name);
         }
+
         public Enemy(string name, int health)
         {
             SetName(name);
@@ -28,26 +77,41 @@
 
         }
 
-        // Function
-        public void SetName(string newName)
+         //function
+         public void SetName(string newname)
+         {
+            if (!string.IsNullOrWhiteSpace(newname))
+                _name = newname;
+         }
+
+        public void SetHealth(int newhealth)
         {
-            if (!string.IsNullOrWhiteSpace(newName))
-                _name = newName;
+            if (int.IsPositive(newhealth) && newhealth <= 100)
+            {
+                _health = newhealth;
+            }
         }
 
-        public void SetHealth(int newHealth)
-        {
-            if (int.IsPositive(newHealth) && newHealth <= 100)
-                _health = newHealth;
-        }
-
-        public string GetName()
+        public string getname()
         { 
             return _name; 
         }
-        public int GetHealth()
+        public int gethealth()
         {
             return _health;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (int.IsPositive(damage))
+            {
+                Health -= damage;
+                if (Health <= 0)
+                {
+                    Health = 0;
+                    isAlive = false;
+                }
+            }
         }
 
     }
