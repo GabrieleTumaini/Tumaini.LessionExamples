759ee3b1aafe190648728cfffa0619619d71ac2c
GabrieleTumaini <gabriele.tumaini.stud@ispascalcomandini.it>
2025-10-17 09:51:14 +0200

    secondo commit del 17/10

---
+++ src/BlasePascal.LessonsExamples.Domain/Hero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.LessonsExamples.Domain
+{
+    internal class Hero
+    {
+        //attributes
+        private string _name;
+        private int _health;
+        private int _level;
+        private int _description;
+
+        //constructors
+        public Hero() 
+        { 
+        }
+        private set
+    }
+}
