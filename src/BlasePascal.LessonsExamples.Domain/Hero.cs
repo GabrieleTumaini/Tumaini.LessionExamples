85a30a70b667f33f1924854e729b565a7ca13c06
GabrieleTumaini <gabriele.tumaini.stud@ispascalcomandini.it>
2025-10-22 10:21:53 +0200

    last commit

--- src/BlasePascal.LessonsExamples.Domain/Hero.cs
+++ src/BlasePascal.LessonsExamples.Domain/Hero.cs
@@ -6,18 +6,75 @@ using System.Threading.Tasks;
 
 namespace BlaisePascal.LessonsExamples.Domain
 {
-    internal class Hero
+    public class Hero
     {
-        //attributes
+        //Attributes
+
         private string _name;
         private int _health;
-        private int _level;
-        private int _description;
+        private string _description;
+        private bool _isAlive;
 
-        //constructors
-        public Hero() 
-        { 
+        //Contructor
+        public Hero()
+        {
         }
-        private set
+        public Hero(string name)
+        {
+            _name = name;
+        }
+
+        public Hero(string name, int health)
+        {
+            _name = name;
+            _health = health;
+        }
+
+        //Functions
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int GetHealth()
+        {
+            return _health;
+        }
+
+
+        public void SetName(string newName)
+        {
+            _name = CharacterValidator.ValidateName(newName);
+        }
+
+        public void SetHealth(int newHealth)
+        {
+            _health = CharacterValidator.ValidateHealth(newHealth);
+        }
+
+        public void isAlive()
+        {
+            _isAlive = _health > 0;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (damage < 0)
+                throw new ArgumentException("Damage cannot be negative");
+
+            _health = Math.Max(CharacterValidator.MinHealth, _health -= damage);
+
+        }
+        public void Heal(int healAmount)
+        {
+            if (healAmount < 0)
+                throw new ArgumentException("Heal amount cannot be negative");
+
+
+            _health = Math.Min(CharacterValidator.MaxHealth, _health += healAmount);
+
+        }
+
     }
 }
